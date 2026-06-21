d457d22b21c4b1d2b7fd19d87297073d153f05d6
hinatahoriba <c0a2212903@edu.teu.ac.jp>
2026-06-21 15:42:44 +0900

    delete: ヘッダーの削除

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import Header from './components/Header.jsx'
 import Profile from './components/Profile.jsx'
 import SocialIcons from './components/SocailIcons.jsx'
 import Skills from './components/Skills.jsx'
@@ -8,7 +7,6 @@ import Works from './components/Works.jsx'
 function App() {
   return (
     <>
-      <Header />
       <Profile />
       <SocialIcons />
       <Skills />
